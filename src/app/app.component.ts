4bcb1407d316b5b887a5c53971865308b7ae0ad4
vitalijs <vitalijs.sakels@28stone.com>
2017-09-12 15:33:39 +0300

    count down

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import * as moment from 'moment'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  nextParty: Date;
+
+  ngOnInit(): void {
+    this.nextParty = this.getNextSeptember2ndSaturday(moment());
+  }
+
+  getSeptember2ndSaturday(startingMoment) {
+    return startingMoment
+      .clone()
+      .month('September')
+      .startOf('month')
+      .day('Saturday')
+      .add(1, 'day')
+      .day('Saturday');
+  }
+
+  getNextSeptember2ndSaturday(startingMoment): Date {
+    var thisYear = this.getSeptember2ndSaturday(startingMoment);
+    if (startingMoment.isAfter(thisYear)) {
+      return this.getSeptember2ndSaturday(startingMoment
+        .clone()
+        .add(1, 'year')
+      ).toDate()
+    }
+    return thisYear.toDate();
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {CountDown} from "ng2-date-countdown";
 
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CountDown
   ],
   imports: [
     BrowserModule
